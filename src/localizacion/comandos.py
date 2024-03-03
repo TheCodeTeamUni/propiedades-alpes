0f1e394bee3561d26481a01287bdbdf2dfa449f8
Edwin Andres Tapia Lasso <e.tapia@uniandes.edu.co>
2024-03-03 11:54:39 -0500

    microservicio localizacion

---
+++ src/localizacion/comandos.py
@@ -0,0 +1,24 @@
+from pulsar.schema import *
+from .utils import time_millis
+import uuid
+
+
+class AgregarUbicacion(Record):
+    id_propiedad = String(),
+    latitud = Double()
+    longitud = Double()
+    
+ 
+
+class ComandoAgregarUbicacion(Record):
+    id = String(default=str(uuid.uuid4()))
+    time = Long()
+    ingestion = Long(default=time_millis())
+    specversion = String(default="v1")
+    type = String(default="AgregarUbicacion")
+    datacontenttype = String()
+    service_name = String(default="ubicacion.aeroalpes")
+    data = AgregarUbicacion
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
