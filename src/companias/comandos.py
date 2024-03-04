01697e87be0332e494351e040cd86ba75e871d25
jpalacio-bv <jose.palacio@bazaarvoice.com>
2024-03-04 11:36:41 -0500

    Se añade ms de compañías

---
+++ src/companias/comandos.py
@@ -0,0 +1,22 @@
+from pulsar.schema import *
+from .utils import time_millis
+import uuid
+
+class AgregarCompania(Record):
+    id_compania = String()
+    nombre = String()
+    telefono = String()
+    direccion_ppal = String()
+
+class ComandoAgregarCompania(Record):
+    id = String(default=str(uuid.uuid4()))
+    time = Long()
+    ingestion = Long(default=time_millis())
+    specversion = String(default="v1")
+    type = String(default="AgregarCompania")
+    datacontenttype = String()
+    service_name = String(default="companias.aeroalpes")
+    data = AgregarCompania
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
